68562219378b9f9c103be983aaabfb93e0d18d6d
sanidhya-sitaula <sanidhya.sitaula1999@gmail.com>
2022-12-30 23:28:39 -0500

    feat: Setup navigation, screens

---
+++ navigation/index.tsx
@@ -0,0 +1,83 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { HomeScreen } from "../screens/HomeScreen";
+import { FontAwesome } from "@expo/vector-icons";
+import { AllBooksScreen } from "../screens/AllBooksScreen";
+import { NotesScreen } from "../screens/NotesScreen";
+import { StatisticsScreen } from "../screens/StatisticsScreen";
+
+export type RootStackParamList = {
+    Root: undefined;
+    Home: undefined;
+    AllBooks: undefined;
+    Notes: undefined;
+    Statistics: undefined;
+};
+
+export default function Navigation() {
+    return (
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    );
+  }
+  
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function RootNavigator() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />  
+      </Stack.Navigator>
+    );
+  }
+  
+  const BottomTab = createBottomTabNavigator<RootStackParamList>();
+
+  function BottomTabNavigator() {
+    return (
+      <BottomTab.Navigator>
+        <BottomTab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ( {color, size} ) => {
+              return <FontAwesome name="home" size={size} color={color} />;
+            },
+          }}
+        />
+        <BottomTab.Screen
+          name="AllBooks"
+          component={AllBooksScreen}
+          options={{
+            tabBarIcon: ({color, size} ) => {
+              return <FontAwesome name="book" size={size} color={color} />;
+            },
+          }}
+        />
+         <BottomTab.Screen
+          name="Notes"
+          component={NotesScreen}
+          options={{
+            tabBarIcon: ({color, size} ) => {
+              return <FontAwesome name="table" size={size} color={color} />;
+            },
+          }}
+        />
+         <BottomTab.Screen
+          name="Statistics"
+          component={StatisticsScreen}
+          options={{
+            tabBarIcon: ({color, size} ) => {
+              return <FontAwesome name="exclamation" size={size} color={color} />;
+            },
+          }}
+        />
+      </BottomTab.Navigator>
+    );
+  }
+
+
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,5 @@
+import { Text, View } from "react-native";
+
+export const HomeScreen = () => {
+    return <View><Text>Home Screen</Text></View>
+}
